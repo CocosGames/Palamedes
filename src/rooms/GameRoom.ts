f7ee01994b90320a43fa5574fa9c6e61510919f1
COCO <support@cocosgames.com>
2022-07-21 18:53:33 +0800

    patterns

--- src/rooms/GameRoom.ts
+++ src/rooms/GameRoom.ts
@@ -82,35 +82,85 @@ export class GameRoom extends Room<GameState> {
         );
 
         this.onMessage("magic", (client, message) => {
-            /*
-            PATTERNS "What have you done makes what are you today."
-
-            pattern 6
-            123456 654321
-            111111 222222 333333 444444 555555 666666
-            4 lines
-
-            pattern 6'
-            xyxyxy xyzxyz
-            3 lines
-
-            pattern 5
-            12345x 23456x 65432x 54321x x12345 x23456 x65432 x54321
-            11111x x11111 22222x x22222 33333x x33333 44444x x44444 55555x x55555 66666x x66666
-            3 lines
-
-            pattern 4
-            1234xx 2345xx 3456xx 6543xx 5432xx 4321xx xx1234 xx2345 xx3456 xx4321 xx5432 xx6543
-            1111xx xx1111 2222xx xx2222 3333xx xx3333 4444xx xx4444 5555xx xx5555 6666xx xx6666
-            2 lines
-
-            pattern 3
-            123xxx 234xxx 345xxx 456xxx 654xxx 543xxx 432xxx 321xxx
-            xxx123 xxx234 xxx345 xxx456 xxx654 xxx543 xxx432 xxx321
-            xxx111 111xxx xxx222 222xxx xxx333 333xxx xxx444 444xxx xxx555 555xxx xxx666 666xxx
-            1 line
-            */
+            console.log("message magic recelived.");
+            let lr = this.patternMagic(this.state.players.get(client.sessionId).history.toJSON());
+            if (lr > 0) {
+                this.state.players.get(client.sessionId).history = new ArraySchema<number>();
+                this.removeLines(client, lr);
+            }
         });
+    }
+
+    /*
+    PATTERNS "What have you done makes what you have today."
+
+    pattern 6
+    123456 654321
+    111111 222222 333333 444444 555555 666666
+    4 lines
+
+    pattern 6'
+    xyxyxy xyzxyz xxxyyy xxyyzz
+    3 lines
+
+    pattern 5
+    12345x 23456x 65432x 54321x x12345 x23456 x65432 x54321
+    11111x x11111 22222x x22222 33333x x33333 44444x x44444 55555x x55555 66666x x66666
+    3 lines
+
+    pattern 4
+    1234xx 2345xx 3456xx 6543xx 5432xx 4321xx xx1234 xx2345 xx3456 xx4321 xx5432 xx6543
+    1111xx xx1111 2222xx xx2222 3333xx xx3333 4444xx xx4444 5555xx xx5555 6666xx xx6666
+    2 lines
+
+    pattern 3
+    123xxx 234xxx 345xxx 456xxx 654xxx 543xxx 432xxx 321xxx
+    xxx123 xxx234 xxx345 xxx456 xxx654 xxx543 xxx432 xxx321
+    xxx111 111xxx xxx222 222xxx xxx333 333xxx xxx444 444xxx xxx555 555xxx xxx666 666xxx
+    1 line
+    */
+    patternMagic(history: number[]): number {
+        let h = history.join("");
+        let pattern6 = ["123456", "654321", "111111", "222222", "333333", "444444", "555555", "666666"];
+        for (var i = 0; i < pattern6.length; i++) {
+            if (h.indexOf(pattern6[i]) >= 0)
+                return 4;
+        }
+        if (h.length==GameRoom.DICE_NUM) {
+            let p6_1 = h.substr(0, 2);
+            if (h == (p6_1 + p6_1 + p6_1))
+                return 3;
+            let p6_2 = h.substr(0, 3);
+            if (h == (p6_2 + p6_2))
+                return 3;
+            let p6_3_1 = h.substr(0, 1);
+            let p6_3_2 = h.substr(1, 1);
+            let p6_3_3 = h.substr(2, 1);
+            let p6_3_4 = h.substr(3, 1);
+            let p6_3_5 = h.substr(4, 1);
+            let p6_3_6 = h.substr(5, 1);
+            if (p6_3_1 == p6_3_2 && p6_3_2 == p6_3_3 && p6_3_4 == p6_3_5 && p6_3_5 == p6_3_6)
+                return 3;
+            if (p6_3_1 == p6_3_2 && p6_3_3 == p6_3_4 && p6_3_5 == p6_3_6)
+                return 3;
+        }
+        let pattern5 = ["12345", "23456", "65432", "54321", "11111", "22222", "33333", "44444", "55555", "66666"];
+        for (var k = 0; k < pattern5.length; k++) {
+            if (h.indexOf(pattern5[k]) >= 0)
+                return 3;
+        }
+        let pattern4 = ["1234", "2345", "3456", "6543", "5432", "4321", "1111", "2222", "3333", "4444", "5555", "6666"];
+        for (var l = 0; l < pattern4.length; l++) {
+            if (h.indexOf(pattern4[l]) >= 0)
+                return 2;
+        }
+        let pattern3 = ["123", "234", "345", "456", "654", "543", "432", "321", "111", "222", "333", "444", "555", "666"];
+        for (var m = 0; m < pattern3.length; m++) {
+            if (h.indexOf(pattern3[m]) >= 0)
+                return 1;
+        }
+
+        return 0;
     }
 
     removeLines(c: Client, l: number = 1) {
